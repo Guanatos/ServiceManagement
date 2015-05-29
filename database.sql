04ad504796b2cb3119c8ce8d6167578e3c9b549f
Daniel Nuno <daniel.nuno@gmail.com>
2015-05-29 18:05:17 -0500

    DB Structure modified, Skills

--- database.sql
+++ database.sql
@@ -36,3 +36,34 @@ CREATE TABLE `wrkr` (
   `wrkr_email` varchar(200) COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`wrkr_id`),
 ) DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci PACK_KEYS=0;
+
+-- --------------------------------------------------------
+-- Table structure 'Skills'
+-- --------------------------------------------------------
+CREATE TABLE `skills` (
+  `skill_id` int(11) NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(100) COLLATE latin1_general_ci NOT NULL,
+  `skill_desc` text COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`wrkr_id`),
+) DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci PACK_KEYS=0;
+
+-- --------------------------------------------------------
+-- Table structure 'Worker's Skills'
+-- --------------------------------------------------------
+CREATE TABLE dept_emp (
+    emp_no      INT         NOT NULL,
+    dept_no     CHAR(4)     NOT NULL,
+    from_date   DATE        NOT NULL,
+    to_date     DATE        NOT NULL,
+    KEY         (emp_no),   -- Build INDEX on this non-unique-value column
+    KEY         (dept_no),  -- Build INDEX on this non-unique-value column
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+           -- Cascade DELETE from parent table 'employee' to this child table
+           -- If an emp_no is deleted from parent 'employee', all records
+           --  involving this emp_no in this child table are also deleted
+           -- ON UPDATE CASCADE??
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
+           -- ON UPDATE CASCADE??
+    PRIMARY KEY (emp_no, dept_no)
+           -- Might not be unique?? Need to include from_date
+) DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci PACK_KEYS=0;
