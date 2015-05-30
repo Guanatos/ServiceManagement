7f2cd4e3d909e295bacdab3cb8801a753b9bdf40
Daniel Nuno <daniel.nuno@gmail.com>
2015-05-30 00:27:58 -0500

    Worker's Skills

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@
 -- Table structure 'Customer'
 -- --------------------------------------------------------
 CREATE TABLE `cstmr` (
-  `cstmr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `cstmr_id` int(10) NOT NULL AUTO_INCREMENT,
   `cstmr_login` varchar(100) COLLATE latin1_general_ci NOT NULL, 
   `cstmr_pass` varchar(225) COLLATE latin1_general_ci DEFAULT NULL,
   `cstmr_address` text COLLATE latin1_general_ci NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE `cstmr` (
 -- Table structure 'Worker'
 -- --------------------------------------------------------
 CREATE TABLE `wrkr` (
-  `wrkr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `wrkr_id` int(10) NOT NULL AUTO_INCREMENT,
   `wrkr_login` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `wrkr_pass` varchar(225) COLLATE latin1_general_ci DEFAULT NULL,
   `wrkr_address` text COLLATE latin1_general_ci NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE `wrkr` (
 -- Table structure 'Skills'
 -- --------------------------------------------------------
 CREATE TABLE `skills` (
-  `skill_id` int(11) NOT NULL AUTO_INCREMENT,
+  `skill_id`   int(5) NOT NULL AUTO_INCREMENT,
   `skill_name` varchar(100) COLLATE latin1_general_ci NOT NULL,
   `skill_desc` text COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`wrkr_id`),
@@ -50,20 +50,13 @@ CREATE TABLE `skills` (
 -- --------------------------------------------------------
 -- Table structure 'Worker's Skills'
 -- --------------------------------------------------------
-CREATE TABLE dept_emp (
-    emp_no      INT         NOT NULL,
-    dept_no     CHAR(4)     NOT NULL,
-    from_date   DATE        NOT NULL,
-    to_date     DATE        NOT NULL,
-    KEY         (emp_no),   -- Build INDEX on this non-unique-value column
-    KEY         (dept_no),  -- Build INDEX on this non-unique-value column
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-           -- Cascade DELETE from parent table 'employee' to this child table
-           -- If an emp_no is deleted from parent 'employee', all records
-           --  involving this emp_no in this child table are also deleted
-           -- ON UPDATE CASCADE??
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
-           -- ON UPDATE CASCADE??
-    PRIMARY KEY (emp_no, dept_no)
-           -- Might not be unique?? Need to include from_date
+CREATE TABLE wrkr_skills (
+    wrkr_id     int(10)    NOT NULL,
+    skill_id    int(5)     NOT NULL,
+    ws_rating   int(1)     NOT NULL,
+    KEY         (wrkr_id), 
+    KEY         (skill_id), 
+    FOREIGN KEY (wrkr_id) REFERENCES wrkr (wrkr_id) ON DELETE CASCADE,
+    FOREIGN KEY (skill_id) REFERENCES skills (skill_id) ON DELETE CASCADE,
+    PRIMARY KEY (wrkr_id, skill_id),
 ) DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci PACK_KEYS=0;
